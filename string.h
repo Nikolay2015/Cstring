c0f3d30c966d7a60a42d9cc6e97091cc7dfec51b
Nikolay2015 <mushroom8613@gmail.com>
2016-01-13 19:07:29 +0300

    rebuilt

--- string.h
+++ string.h
@@ -16,57 +16,47 @@ public:
 
 	String(const char *str)
 	{
-		size_ = 0;
-		const char *str_size = str;
-		while (*str_size != '\0')
-		{
-			size_ += 1;
-			str_size = (str_size + 1);
-		}
-		char *temp = new char[size_ + 1];
-		while (*(temp++) = *(str++));
-		temp = (temp - size_ - 1);
-		str_ = temp;
+		size_ = std::strlen(str);
+		str_ = new char[size_ + 1];
+		while (*(str_++) = *(str++));
+		str_ = (str_ - size_ - 1);
 	};
 
 	String(const char *str, unsigned count)
 	{
-		char *temp = new char[count + 1];
+		str_ = new char[count + 1];
 		const char *str_temp = str;
 		for (unsigned i = 0; i < count; i++)
 		{
-			temp[i] = *str_temp;
-			str_temp = (str_temp + 1);
+			str_[i] = *str_temp;
+			str_temp++;
 		}
-		temp[count] = '\0';
+		str_[count] = '\0';
 		size_ = count;
-		str_ = temp;
 	};
 
 	String(char ch, unsigned count)
 	{
-		char *temp = new char[count + 1];
+		str_ = new char[count + 1];
 		for (unsigned i = 0; i < count; i++)
 		{
-			temp[i] = ch;
+			str_[i] = ch;
 		}
-		temp[count] = '\0';
+		str_[count] = '\0';
 		size_ = count;
-		str_ = temp;
 	};
 
 	String(const String &other)
 	{
 		size_ = other.size_;
 		char *str_temp = other.str_;
-		char *temp = new char[other.size_ + 1];
+		str_ = new char[other.size_ + 1];
 		for (unsigned i = 0; i < other.size_; i++)
 		{
-			temp[i] = *str_temp;
+			str_[i] = *str_temp;
 			str_temp = (str_temp + 1);
 		}
-		temp[other.size_] = '\0';
-		str_ = temp;
+		str_[other.size_] = '\0';
 	};
 
 	String(String &&other)
@@ -74,7 +64,7 @@ public:
 		size_ = other.size_;
 		str_ = other.str_;
 		other.size_ = 0;
-		other.str_ = NULL;
+		other.str_ = nullptr;
 	};
 
 	~String()
@@ -82,7 +72,7 @@ public:
 		delete[] str_;
 	};
 
-	String &operator =(const String &other)
+	String &operator=(const String &other)
 	{
 		size_ = other.size_;
 		char *str_temp = other.str_;
@@ -93,70 +83,47 @@ public:
 			str_temp = (str_temp + 1);
 		}
 		temp[other.size_] = '\0';
+		delete[] str_;
 		str_ = temp;
 		return *this;
 	};
 
-	String &operator =(String &&other)
+	String &operator=(String &&other)
 	{
+		delete[] str_;
 		size_ = other.size_;
 		str_ = other.str_;
 		other.size_ = 0;
-		other.str_ = NULL;
+		other.str_ = nullptr;
 		return *this;
 	};
 
-	String &operator +=(const String &suffix)
+	String &operator+=(const String &suffix)
 	{
 		char *str;
 		char *s = str_;
 		char *temp = str = new char[size_ + suffix.size_ + 1];
-
-		for (unsigned i = 0; i < size_; i++) 
-		{
-			*(temp++) = *(s++);
-		}
-		
 		char *temp_str = suffix.str_;
-		for (unsigned i = 0; i < suffix.size_; i++)
-		{
-			*(temp++) = *(temp_str++);
-		}
-		delete[] str_;
-		*temp = '\0';
+		copy_suffix(temp, s, suffix.size_, temp_str);
 		size_ += suffix.size_;
 		str_ = str;
 		return *this;
 	};
 
-	String &operator +=(const char * suffix)
+	String &operator+=(const char * suffix)
 	{
-		unsigned size = 0;
+		unsigned size = std::strlen(suffix);
 		const char *temp_suffix = suffix;
-		while (*(temp_suffix++) != '\0') //size of suffix
-		{
-			size++;
-		}
-		temp_suffix = suffix;
 		char *str;
 		char *s = str_;
 		char *temp = str = new char[size_ + size + 1];
-		for (unsigned i = 0; i < size_; i++)// copy of str_ 
-		{
-				*(temp++) = *(s++);
-		}
-		for (unsigned i = 0; i < size; i++)//copy of suffix
-		{
-			*(temp++) = *(temp_suffix++);
-		}
-		*temp = '\0';
-		delete[] str_;
+		copy_suffix(temp, s, size, temp_suffix);
 		size_ += size;
 		str_ = str;
 		return *this;
 	};
 
-	String &operator +=(char suffix)
+	String &operator+=(char suffix)
 	{
 		char *str;
 		char *s = str_;
@@ -168,38 +135,32 @@ public:
 		*(temp++) = suffix;
 		*temp = '\0';
 		delete[] str_;
-		size_ = size_ + 1;
+		size_++;
 		str_ = str;
 		return *this;
 	};
 
-	void swap(String &other) 
+	void swap(String &other)
 	{
-		char *temp;
-		unsigned size;
-		temp = str_;
-		size = size_;
-		str_ = other.str_;
-		size_ = other.size_;
-		other.str_ = temp;
-		other.size_ = size;
+		std::swap(str_, other.str_);
+		std::swap(size_, other.size_);
 	};
 
-	char &operator [](unsigned pos) 
+	char &operator[](unsigned pos)
 	{
 		return *(str_ + pos);
 	};
 
-	const char operator [](unsigned pos) const 
+	const char operator[](unsigned pos) const
 	{
 		return *(str_ + pos);
 	};
 
-	char & at(unsigned pos) 
+	char &at(unsigned pos)
 	{
-		if (pos >= size_) 
+		if (pos >= size_)
 		{
-			throw std::out_of_range("");
+			throw std::out_of_range("position > your size");
 		}
 		char *temp_pos = (str_ + pos);
 		return *(temp_pos);
@@ -215,19 +176,19 @@ public:
 		return size_;
 	};
 
-	friend std::ostream &operator <<(std::ostream &stream, const String &s)
+	friend std::ostream &operator<<(std::ostream &stream, const String &s)
 	{
 		stream << s.data();
 		return stream;
 	};
 
-	friend bool operator ==(const String &lhs, const String &rhs) 
+	friend bool operator==(const String &lhs, const String &rhs)
 	{
 		if (lhs.size_ == rhs.size_) // if size equal then 
 		{
 			char *temp_left = lhs.str_;
 			char *temp_right = rhs.str_;
-			for (unsigned i = 0; i <= lhs.size_; i++) 
+			for (unsigned i = 0; i <= lhs.size_; i++)
 			{
 				if (*(temp_left++) != *(temp_right++))// if not equal then false
 				{
@@ -237,26 +198,26 @@ public:
 			return true;
 		}
 		else
-		{ 
+		{
 			return false;
 		}
-		
+
 	};
 
-	friend bool operator <(const String &lhs, const String &rhs) 
+	friend bool operator<(const String &lhs, const String &rhs)
 	{
 		unsigned size = lhs.size_;
 		if (size <= rhs.size_)
 		{
 			size = rhs.size_;
 		}
-		for (unsigned i = 0; i < size; i++) 
+		for (unsigned i = 0; i < size; i++)
 		{
-			if (*(lhs.str_ + i) < *(rhs.str_ + i)) 
+			if (*(lhs.str_ + i) < *(rhs.str_ + i))
 			{
 				return true;
 			}
-			else if (*(lhs.str_ + i) > *(rhs.str_ + i)) 
+			else if (*(lhs.str_ + i) > *(rhs.str_ + i))
 			{
 				return false;
 			}
@@ -264,42 +225,70 @@ public:
 		return false;
 	};
 
-private :
+private:
 	unsigned int size_;
 	char *str_;
+
+	void copy_suffix(char *temp, char *s, unsigned size, const char *temp_suffix)
+	{
+		for (unsigned i = 0; i < size_; i++)// copy of str_ 
+		{
+			*(temp++) = *(s++);
+		}
+		for (unsigned i = 0; i < size; i++)//copy of suffix
+		{
+			*(temp++) = *(temp_suffix++);
+		}
+		*temp = '\0';
+		delete[] str_;
+	};
+
+	void copy_suffix(char *temp, char *s, unsigned size, char *temp_suffix)
+	{
+		for (unsigned i = 0; i < size_; i++)// copy of str_ 
+		{
+			*(temp++) = *(s++);
+		}
+		for (unsigned i = 0; i < size; i++)//copy of suffix
+		{
+			*(temp++) = *(temp_suffix++);
+		}
+		*temp = '\0';
+		delete[] str_;
+	};
 };
 
-String operator +(const String &lhs, const String &rhs)
+String operator+(const String &lhs, const String &rhs)
 {
 	return String(lhs) += rhs;
 };
 
-String operator +(const String &lhs, const char *rhs)
+String operator+(const String &lhs, const char *rhs)
 {
 	return String(lhs) += rhs;
 };
 
-String operator +(const char *lhs, const String &rhs)
+String operator+(const char *lhs, const String &rhs)
 {
 	return String(lhs) += rhs;
 };
 
-bool operator !=(const String &lhs, const String &rhs)
+bool operator!=(const String &lhs, const String &rhs)
 {
 	return !(lhs == rhs);
 };
 
-bool operator <=(const String &lhs, const String &rhs)
+bool operator<=(const String &lhs, const String &rhs)
 {
 	return (lhs == rhs) || (lhs < rhs);
 };
 
-bool operator >(const String &lhs, const String &rhs)
+bool operator>(const String &lhs, const String &rhs)
 {
 	return !(lhs <= rhs);
 };
 
-bool operator >=(const String &lhs, const String &rhs)
+bool operator>=(const String &lhs, const String &rhs)
 {
 	return !(lhs < rhs);
 };
